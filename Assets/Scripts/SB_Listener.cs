046fd6224b01d595710c63d0be4064e83f40b77a
Peiki <peikimo@hotmail.it>
2017-03-02 18:34:14 +0100

    Adjusted Settings, Creating UI and Base

---
+++ Assets/Scripts/SB_Listener.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SB_Listener:MonoBehaviour{
+	public void PrintToScreen(){
+		Debug.Log("TESTING");
+	}
+}
