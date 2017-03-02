e980871a4909c09eef09baff3aff8c97c2c281f2
Peiki <peikimo@hotmail.it>
2017-03-02 22:13:57 +0100

    Implementing Shooting

--- Assets/Scripts/SB_Listener.cs
+++ Assets/Scripts/SB_Listener.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class SB_Listener:MonoBehaviour{
-	public void PrintToScreen(){
-		Debug.Log("TESTING");
+	[SerializeField] GameObject weapon;
+	public Rigidbody2D bullet;
+	public float maxSpeed=300f;
+	Vector3 direction;
+	public void Start(){
+		Vector3 direction=weapon.transform.forward;
 	}
+	public void Shoot(){
+		bullet=Instantiate(bullet,weapon.transform.position,weapon.transform.rotation) as Rigidbody2D; //position to change at top of arrow
+		bullet.AddForce(new Vector2(maxSpeed,0),ForceMode2D.Impulse);
+	}
+	 void OnTriggerEnter(Collider other){ //TESTING
+        Destroy(bullet);
+    }
+
+    /*void OnColissionEnter(Collision col) {
+         if(col.collider.tag == "ground")
+             isGrounded = true;
+    	*/
 }
