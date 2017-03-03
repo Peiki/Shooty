3987f12af598ce412ee8edb0b33c4db7c908f1eb
Peiki <peikimo@hotmail.it>
2017-03-03 18:57:14 +0100

    Created Menu and Popup

---
+++ Assets/Scripts/MenuListener.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuListener:MonoBehaviour{
+	public void StartGame(){
+		SceneManager.LoadScene("Game",LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scripts/PopupScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PopupScript:MonoBehaviour{
+	void Start(){
+		gameObject.SetActive(false);
+	}
+	public void Activate(){
+		gameObject.SetActive(true);
+	}
+	public void Deactivate(){
+		gameObject.SetActive(false);
+	}
+	public void GoToMenu(){
+		SceneManager.LoadScene("Menu",LoadSceneMode.Single);
+	}
+}
