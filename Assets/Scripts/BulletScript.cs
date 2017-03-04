de98d2141a91b113537077c0df3ad0a5a6cbf1df
Peiki <peikimo@hotmail.it>
2017-03-04 18:49:15 +0100

    Perfectioning Shooting

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BulletScript:MonoBehaviour{
+	public float bulletSpeed=0.001f;
+	Vector2 direction;
+	bool hit=false;
+	void Update(){
+		transform.Translate(direction*Time.deltaTime*bulletSpeed);
+		if(transform.position.x==direction.x && transform.position.y==direction.y)
+			hit=!hit;
+
+	}
+	public void setDirection(Vector2 direction){
+		this.direction=direction;
+	}
+	public bool getHit(){
+		return hit;
+	}
+}
--- Assets/Scripts/SB_Listener.cs
+++ Assets/Scripts/SB_Listener.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SB_Listener:MonoBehaviour{
 	[SerializeField] GameObject weapon;
-	public Rigidbody2D bullet;
+	[SerializeField] GameObject bullet;
 	public float maxSpeed=300f;
+	public int distance=5;
+	public float angle;
+
 	public void Shoot(){
+		angle=weapon.transform.rotation.z*2.3f;
+
 		Vector2 position=new Vector2(weapon.transform.position.x,weapon.transform.position.y);
-		Vector2 direction=new Vector2(0,8);
-		RaycastHit2D hit=Physics2D.Raycast(position,direction,200);
-		Debug.DrawLine(position,direction);
-		if(hit.collider!=null){
+		Vector2 direction=new Vector2(-(distance*Mathf.Sin(angle)),distance*Mathf.Cos(angle));
+
+		RaycastHit2D hit=Physics2D.Raycast(position,direction,maxSpeed);
+
+		Instantiate(bullet,position,Quaternion.identity).GetComponent<BulletScript>().setDirection(direction); //LookRotation?
+
+		if(hit.collider!=null)
 			Destroy(hit.collider.gameObject);
-		}		
+			/*
+			Debug.Log(hit.collider.gameObject.GetComponent<BulletScript>().getHit());
+			while(!(hit.collider.gameObject.GetComponent<BulletScript>().getHit())){
+
+			}
+			*/
 	}
 }
