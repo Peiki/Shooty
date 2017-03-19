e3dff373522606d28282aec8d98853cc1bc3806b
Peiki <peikimo@hotmail.it>
2017-03-19 17:28:23 +0100

    Added Monster Types

---
+++ Assets/Scripts/DiagonalMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DiagonalMovement:MonoBehaviour{
+	float speed;
+	Vector3 direction;
+	void Start(){
+		speed=GetComponent<MonsterBehaviour>().getSpeed();
+		randomDirection();
+	}
+	void Update(){
+		if(!GetComponent<MonsterBehaviour>().getDead())
+			transform.Translate(direction*speed*Time.deltaTime);
+		if(transform.position.x<-5.10 || transform.position.x>5.10)
+			changeDirection();
+	}
+	void randomDirection(){
+		int value=Random.Range(1,3);
+		if(value==1)
+			direction=Vector3.right;
+		else
+			direction=Vector3.left;
+	}
+	void changeDirection(){
+		if(direction==Vector3.right)
+			direction=Vector3.left;
+		else if(direction==Vector3.left)
+			direction=Vector3.right;
+	}
+}
---
+++ Assets/Scripts/HideScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class HideScript:MonoBehaviour{
+	[SerializeField] Sprite hidden;
+	[SerializeField] Sprite shown;
+	bool state=false;
+	bool passed=false;
+	void Start(){
+		StartCoroutine(HideRandomly());
+	}
+	void Update(){
+		if(transform.position.y<-3.5f && !passed){
+			changeState(true);
+			passed=true;
+		}
+	}
+	void changeState(bool state){
+		if(state){
+			GetComponent<SpriteRenderer>().sprite=shown;
+			gameObject.AddComponent<BoxCollider2D>().isTrigger=true;
+		}
+		else{
+			GetComponent<SpriteRenderer>().sprite=hidden;
+			Destroy(GetComponent<BoxCollider2D>());
+		}
+	}
+	private IEnumerator HideRandomly(){
+		while(!GetComponent<MonsterBehaviour>().getDead()){
+			yield return new WaitForSeconds(Random.Range(0.5f,3));
+			if(transform.position.y>=-3.5f){
+				changeState(state);
+				state=!state;
+			}
+		}
+	}
+}
